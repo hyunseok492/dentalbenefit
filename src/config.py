acd096c193e5f57884871c87a658bae3b54f46b1
hyunseok492 <jejune66@gmail.com>
2024-12-27 12:30:39 +0900

    Update config.py

--- src/config.py
+++ src/config.py
@@ -7,6 +7,6 @@ OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 OPENAI_MODEL = "gpt-3.5-turbo"  # 혹은 원하는 모델명
 EMBEDDING_MODEL = "text-embedding-ada-002"
 XML_API_KEY = os.environ.get("XML_API_KEY")
+XML_API_URL = os.environ.get("XML_API_URL")
 XML_API_URL = "http://apis.data.go.kr/B551182/NdrgStdInfoService"
 XML_API_URL = "http://apis.data.go.kr/B551182/drgSeparateCompensationItemListService1"
-XML_API_URL = "http://apis.data.go.kr/B551182/mdfeeCrtrInfoService"
